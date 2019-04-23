5cd7247c66e5b3e53ad055e5eddcdc304bafe4c8
___tobbe <tobias@tobiasolsson.se>
2019-04-23 11:38:35 +0200

    börjat med checkin och checkout date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BookingDateComponent } from './booking-date/booking-date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingDateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/booking-date/booking-date.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'booking-date',
+  templateUrl: './booking-date.component.html',
+  styleUrls: ['./booking-date.component.css']
+})
+export class BookingDateComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
