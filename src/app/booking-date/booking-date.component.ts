217e529f196e89c7d5cd86147419bda4d7ad8517
___tobbe <tobias@tobiasolsson.se>
2019-04-23 14:31:52 +0200

    countNights funkar

--- src/app/booking-date/booking-date.component.ts
+++ src/app/booking-date/booking-date.component.ts
@@ -1,31 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
 @Component({
   selector: 'booking-date',
   templateUrl: './booking-date.component.html',
   styleUrls: ['./booking-date.component.css']
 })
-
-
 export class BookingDateComponent implements OnInit {
-  const now:date  = Date.now();
-  public todaysDate: string =  "2019-04-23";  
-  checkInDate: date;
-  checkOutDate: date;
+  dateFormat : string = 'YYYY-MM-DD'
+  checkInDate: string ;
+  checkOutDate: string ;
+  checkOutMinDate: string;
+  minDate = moment(new Date()).format(this.dateFormat);
 
   constructor() { }
   
   ngOnInit() {
-   console.log(this.now);
+    var a = moment('2016-06-06');//now
+var b = moment('2016-05-06');
+    console.log(a.diff(b, 'days')) // 31
+
   }
   
-  onUpdateCheckInDate(event: event){
-    this.checkInDate = (<HTMLInputElement>event.target).value;
-    console.log("check in date changed", this.checkInDate);
+  onUpdateCheckInDate(event: Event) {
+    this.checkInDate = ( event.target as HTMLInputElement).value;
+    this.checkOutMinDate = moment(this.checkInDate).add({days: 1}).format(this.dateFormat);
+    console.log(this.countNights(this.checkInDate,this.checkOutDate));
   }
-  
-  onUpdateCheckOutDate(event: event){
-    this.checkOutDate = (<HTMLInputElement>event.target).value;
-    console.log("check out date changed", this.checkOutDate);
+  onUpdateCheckOutDate(event: Event) {
+    this.checkOutDate = ( event.target as HTMLInputElement).value;
+    console.log(this.countNights(this.checkInDate,this.checkOutDate));
+  }
+  private countNights(inDate: string, outDate: string){
+    const a = moment(outDate);
+    const b = moment(inDate);
+    return a.diff(b, 'days');
   }
 }
