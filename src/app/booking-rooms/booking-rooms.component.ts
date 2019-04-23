d4d6452d9f614f1b018ccfdca7389aeb54d40e06
Tobias Olsson <tobias@tobiasolsson.se>
2019-04-23 19:36:24 +0200

    input output

--- src/app/booking-rooms/booking-rooms.component.ts
+++ src/app/booking-rooms/booking-rooms.component.ts
@@ -36,4 +36,7 @@ export class BookingRoomsComponent implements OnInit {
 
   ngOnInit() {
   }
+  toggleAsActiveRoom(detailCmp: Room) {
+    console.log('toggle: ',detailCmp);
+  }
 }
--- src/app/booking-rooms/room/room.component.ts
+++ src/app/booking-rooms/room/room.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Room } from '../room.model';
 @Component({
   selector: 'room',
@@ -7,11 +7,10 @@ import { Room } from '../room.model';
 })
 export class RoomComponent implements OnInit {
   @Input('room') room: Room;
+  @Output() toggleAsActiveRoom = new EventEmitter<Room>();
+
   constructor() { }
 
   ngOnInit() {
-  }
-  onSetActiveRoom(event: Event){
-    console.log('active room: ', event );
   }
 }
