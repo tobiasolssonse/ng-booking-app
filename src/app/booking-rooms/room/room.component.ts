fb676dbd142750e4e7909b5a1f537f167a7acdb7
___tobbe <tobias@tobiasolsson.se>
2019-04-24 10:10:20 +0200

    mer med emit

--- src/app/booking-rooms/room/room.component.ts
+++ src/app/booking-rooms/room/room.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Room } from '../room.model';
 @Component({
-  selector: 'room',
+  selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css']
 })
 export class RoomComponent implements OnInit {
   @Input('room') room: Room;
   @Output() change = new EventEmitter<Room>();
-  status : boolean;
+  status: boolean;
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +16,10 @@ export class RoomComponent implements OnInit {
   toggleAsActiveRoom(){
     console.log('ROOM.COMPONENT: clicked');
     this.status = !this.status;
-    this.change.emit();
+    this.change.emit({
+      'name' : 'testnamn',
+      'description' : 'test desc',
+      'imagePath' : 'fake imagepath',
+    });
   }
 }
