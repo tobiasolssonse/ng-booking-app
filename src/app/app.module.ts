12f7bb815489fa74d1a85d71b282aa36fcd9d45b
Tobias Olsson <tobias@tobiasolsson.se>
2019-04-28 19:50:14 +0200

    börjat med formuläret

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BookingDateComponent } from './booking-date/booking-date.component';
-
 import { BookingRoomsComponent } from './booking-rooms/booking-rooms.component';
 import { RoomComponent } from './booking-rooms/room/room.component';
-
 import { BookingRoomsService } from './booking-rooms/booking-rooms.service';
 import { BookingFormComponent } from './booking-form/booking-form.component';
 
@@ -21,7 +19,8 @@ import { BookingFormComponent } from './booking-form/booking-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [
     BookingRoomsService
--- src/app/booking-form/booking-form.component.ts
+++ src/app/booking-form/booking-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-booking-form',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking-form.component.css']
 })
 export class BookingFormComponent implements OnInit {
-
+  bookingForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.bookingForm = new FormGroup({
+      name : new FormControl(null, Validators.required),
+      email : new FormControl(null, [Validators.required, Validators.email]),
+    });
   }
-
+  onSubmit() {
+    console.log(this.bookingForm);
+  }
 }
