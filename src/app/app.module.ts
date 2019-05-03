361eda06400f9a8255e03e52c8ec401b727e62ea
___tobbe <tobias@tobiasolsson.se>
2019-05-03 14:53:52 +0200

    oneform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, SatDatepickerModule } f
 import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
 
 import { AppComponent } from './app.component';
-import { BookingDateComponent } from './booking-date/booking-date.component';
 import { BookingRoomsComponent } from './booking-rooms/booking-rooms.component';
 import { RoomComponent } from './booking-rooms/room/room.component';
 import { BookingRoomsService } from './shared/booking-rooms.service';
@@ -21,7 +20,6 @@ import { BookingFormComponent } from './booking-form/booking-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BookingDateComponent,
     BookingRoomsComponent,
     RoomComponent,
     BookingFormComponent,
--- src/app/booking-form/booking-form.component.ts
+++ src/app/booking-form/booking-form.component.ts
@@ -1,22 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatOptionSelectionChange} from '@angular/material';
 
+import { Room } from '../shared/room.model';
+import { Booking } from '../shared/booking.model';
+import { BookingRoomsService } from '../shared/booking-rooms.service';
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css']
 })
 export class BookingFormComponent implements OnInit {
-  bookingForm: FormGroup;
-  constructor() { }
+  rooms: Room[];
+  selectedRoom: Room;
+  booking: Booking;
+  form: FormGroup;
+  constructor(private bookingRoomsService: BookingRoomsService, fb: FormBuilder) {
+    this.form = fb.group({
+      date: [
+        {
+          begin: bookingRoomsService.inDate,
+          end: bookingRoomsService.inDate
+        }
+      ]
+    });
+  }
 
   ngOnInit() {
-    this.bookingForm = new FormGroup({
+    this.form = new FormGroup({
       name : new FormControl(null, Validators.required),
       email : new FormControl(null, [Validators.required, Validators.email]),
     });
+    this.rooms = this.bookingRoomsService.getRooms();
+    this.booking = this.bookingRoomsService.getBooking();
+    console.log(this.booking);
+  }
+  onChangeRoom(event: MatOptionSelectionChange) {
+    // console.log(event);
+    this.bookingRoomsService.updateBooking({ room : event} );
   }
   onSubmit() {
-    console.log(this.bookingForm);
+    console.log(this.form);
   }
 }
