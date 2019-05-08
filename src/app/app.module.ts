a1e1e76f31cfdcbcb6d92f2be92e125b949743a4
___tobbe <tobias@tobiasolsson.se>
2019-05-08 11:05:13 +0200

    td-form på gång

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MatFormFieldModule } from '@angular/material/form-field';
+// MATERIAL
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, SatDatepickerModule } from 'saturn-datepicker';
 import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
-
+//APP
 import { AppComponent } from './app.component';
 import { BookingRoomsService } from './shared/booking-rooms.service';
 import { BookingFormComponent } from './booking-form/booking-form.component';
@@ -22,7 +23,6 @@ import { BookingFormComponent } from './booking-form/booking-form.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
     MatDatepickerModule,
--- src/app/booking-form/booking-form.component.ts
+++ src/app/booking-form/booking-form.component.ts
@@ -1,37 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatOptionSelectionChange} from '@angular/material';
 
 import { BookingRoomsService } from '../shared/booking-rooms.service';
 import { Room } from '../shared/room.model';
 import { Booking } from '../shared/booking.model';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css']
 })
 export class BookingFormComponent implements OnInit {
-  bookingForm: FormGroup;
+  @ViewChild('f') bookingForm: NgForm;
   rooms: Room[];
-  selectedRoom: Room;
   booking: Booking;
+  defaultRoom = 'Standard double';
   constructor(private bookingRoomsService: BookingRoomsService) {
   }
   ngOnInit() {
     this.rooms = this.bookingRoomsService.getRooms();
     this.booking = this.bookingRoomsService.getBooking();
-    this.bookingForm = new FormGroup({
-      'name': new FormControl(null),
-      'email': new FormControl(null),
-      'date': new FormControl(null),
-      // name : new FormControl(null, Validators.required),
-      // email : new FormControl(null, [Validators.required, Validators.email]),
-      // date : new FormControl(null, Validators.required),
-    });
-    console.log(this.booking);
   }
   onChangeRoom(event: MatOptionSelectionChange) {
-    this.bookingRoomsService.updateBooking({ room : event} );
+    this.bookingRoomsService.updateBooking({ room: event} );
   }
   onSubmit() {
     console.log(this.bookingForm);
