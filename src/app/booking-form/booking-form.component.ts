f86650fa0ceea17e95b79fcb0f02ca34d9fc872d
___tobbe <tobias@tobiasolsson.se>
2019-05-14 09:41:31 +0200

    mer mer

--- src/app/booking-form/booking-form.component.ts
+++ src/app/booking-form/booking-form.component.ts
@@ -3,7 +3,7 @@ import { MatOptionSelectionChange} from '@angular/material';
 import { BookingRoomsService } from '../shared/booking-rooms.service';
 import { Room } from '../shared/room.model';
 import { NgForm } from '@angular/forms';
-import {MatDatepickerInputEvent} from '@angular/material/datepicker'
+import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
@@ -13,14 +13,14 @@ export class BookingFormComponent implements OnInit {
   @ViewChild('f') bookingForm: NgForm;
   rooms: Room[];
   today = new Date();
-  dateIn:any = new Date();
-  dateOut = new Date(this.dateIn - (-24*60*60*1000));
+  dateIn: any = new Date();
+  dateOut = new Date(this.dateIn - (-24 * 60 * 60 * 1000));
 
   defaultRoom = 'Standard double';
   constructor(private bookingRoomsService: BookingRoomsService) {
   }
   ngOnInit() {
-    this.bookingRoomsService.getRooms().subscribe(actionArray =>{
+    this.bookingRoomsService.getRooms().subscribe(actionArray => {
       this.rooms = actionArray.map(item => {
         return {
           ...item.payload.doc.data()
