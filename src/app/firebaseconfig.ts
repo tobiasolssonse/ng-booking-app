1ac3c75432b779b0a2c311a39e497791e570b856
Tobias olsson <tobias@tobiasolsson.se>
2019-06-13 10:40:03 +0200

    Delete firebaseconfig.ts

--- src/app/firebaseconfig.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyDxY4dKVfCOSCgsjmbKzeUlX0idSwkyI1A',
-    authDomain: 'ng-booking-app-9b2a1.firebaseapp.com',
-    databaseURL: 'https://ng-booking-app-9b2a1.firebaseio.com',
-    projectId: 'ng-booking-app-9b2a1',
-    storageBucket: 'ng-booking-app-9b2a1.appspot.com',
-    messagingSenderId: '230619374613',
-    appId: '1:230619374613:web:758917ba69bed4fa'
-};
